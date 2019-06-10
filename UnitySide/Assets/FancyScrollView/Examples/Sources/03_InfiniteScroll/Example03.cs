03a0b5c609d3d6a83f952faeb16bd0efb535ecd8
nasuton <lovetennis_airu@yahoo.co.jp>
2019-06-10 23:09:19 +0900

    FancyScrollViewの追加

---
+++ UnitySide/Assets/FancyScrollView/Examples/Sources/03_InfiniteScroll/Example03.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FancyScrollView.Example03
+{
+    public class Example03 : MonoBehaviour
+    {
+        [SerializeField] ScrollView scrollView = default;
+
+        void Start()
+        {
+            var items = Enumerable.Range(0, 20)
+                .Select(i => new ItemData($"Cell {i}"))
+                .ToArray();
+
+            scrollView.UpdateData(items);
+            scrollView.SelectCell(0);
+        }
+    }
+}
